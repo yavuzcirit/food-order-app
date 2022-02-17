63b4a9e6b2666afd2653ec9914bc4ac8660df8f9
yavuzcirit <yavuzcirit23@gmail.com>
2022-02-18 00:42:03 +0300

    header section completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{ Fragment} from 'react';
+import Header from './components/Layout/Header'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <h2>Let's get started!</h2>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './Header.module.css'
+import img from '../../assets/meals.jpeg'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header} >
+        <h1>AsafSteak</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={img} alt='meals_on_table'/>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Header;
