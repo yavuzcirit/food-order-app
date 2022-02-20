cc9ae9cc40fe1fbfc15eb3b4df400d20777d5c42
yavuzcirit <yavuzcirit23@gmail.com>
2022-02-21 01:00:13 +0300

    meals section completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React,{ Fragment} from 'react';
 import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
       <h2>Let's get started!</h2>
     </Fragment>
   );
